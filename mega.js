1c252c5c3e5c0fb9f7fcce28e50c2f7ef86c0f37
🅂𝙞𝙡𝙚𝙣𝙩 𝙠𝙞𝙡𝙡𝙚𝙧 <166059547+HANDSOMKILLER@users.noreply.github.com>
2024-11-14 08:31:32 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'silentkilleer09@gmail.com ', //silentkilleer09@gmail.com 
-    password: 'aw4153469', //aw4143469
+    email: 'mr.jani9292821@gmail.com', //silentkilleer09@gmail.com 
+    password: 'aw4143469', //aw4143469
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
